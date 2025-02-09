e91cc11e1504b533412ec4143fc271d0415357a8
Samuel Rangel <samuel.rangel.021@gmail.com>
2025-02-09 02:00:43 -0400

    Creating navigation pages

--- main.py
+++ main.py
@@ -1,20 +1,58 @@
 import streamlit as st
 
+# ----- Page setup -----
 
-st.title('_Testing Repository_')
+home_page = st.Page(
+    page='sections/Home.py',
+    title='Home',
+    default=True
+)
+math_page = st.Page(
+    page='sections/Math.py',
+    title='Math',
+)
+chem_page = st.Page(
+    page='sections/Chem.py',
+    title='Chem',
+)
+stats_page = st.Page(
+    page='sections/Stats.py',
+    title='Stats',
+)
 
 
-back_color = 'primary'
+# ----- Navigation ---------
 
-if st.button('For multiple questions',type=back_color):
-    st.code('Per policy of the platform, I can only help you with one question per session')
-    st.code("""Per policy of the platform, I can only help you with one part of the question per session.
-                Unless you tell me otherwise, I'll start and help you with the first one""")
+pages = st.navigation(pages=
+    {
+        '':[home_page],
+        'Topics':[chem_page,math_page,stats_page]
+        }
+        )
 
-if st.button('For direct answer requested', type=back_color):
-    st.code('Per policy of the platform, I can not give you the answer without a complete explanation')
 
-if st.button('Ending message',type=back_color):
-    st.code("Please check and let me know if you have any questions.")
-    st.code("If you're satisfied with the explanation provided, please don't forget to rate accordingly :)")
+# -------Run Navigation-------
+
+if __name__== '__main__':
+
+    pages.run()
+
+    st.sidebar.text('Made by Samuel Rangel')
+
+    col1, col2, col3 = st.columns(3)
+    back_color = 'secondary'
+
+    if col1.button('For multiple questions',type=back_color, use_container_width=True):
+        st.code('Per policy of the platform, I can only help you with one question per session')
+        st.code("""Per policy of the platform, I can only help you with one part of the question per session.
+            Unless you tell me otherwise, I'll start and help you with the first one""")
+        
+    if col2.button('For direct answer requested', type=back_color, use_container_width=True):
+        st.code('Per policy of the platform, I can not give you the answer without a complete explanation')
+
+    if col3.button('Ending message',type=back_color, use_container_width=True):
+        st.code("Please check and let me know if you have any questions.")
+        st.code("If you're satisfied with the explanation provided, please don't forget to rate accordingly :)")
+
+    
 
---
+++ sections/Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title('_Science and Math Assistant_')
+
+st.header('Revisar Herencia, DataClass y Abstract class y aplicarlo si conviene')
+st.header('Rediseñar la class constant')
+st.header('En Gas Laws: añadir un RaiseError para cuando las unidades no sean iguales, y/o la temperatura no este en Kelvin')
+st.subheader('Buscar como leer txt para escribir compuesto y tener molar mass')
+
